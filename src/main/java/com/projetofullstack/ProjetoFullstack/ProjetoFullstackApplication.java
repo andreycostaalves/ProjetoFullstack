7032ec57268978b826512a3862c2a2c9bb835be6
andrey alves <andreyalvesgit@gmail.com>
2019-05-27 15:15:11 -0300

    Projeto Criado

---
+++ src/main/java/com/projetofullstack/ProjetoFullstack/ProjetoFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.projetofullstack.ProjetoFullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFullstackApplication.class, args);
+	}
+
+}
